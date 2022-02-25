9e28f63dcb57076839d849a45a0869a873a61f72
Tom_The_Geek <tgbryan39@gmail.com>
2022-02-25 14:05:49 +0000

    feat: Connect 4 game

---
+++ src/main.ts
@@ -0,0 +1,251 @@
+import './style.css';
+import '@fontsource/poppins';
+
+type Colour = 'red' | 'yellow';
+type Grid = [Colour[], Colour[], Colour[], Colour[], Colour[], Colour[], Colour[]];
+
+function getColumnElement(idx: number): HTMLDivElement {
+  return document.querySelector<HTMLDivElement>(`#col-${idx}`)!;
+}
+
+const gridContainer = document.querySelector<HTMLDivElement>('.grid-container')!;
+const gridEle = document.querySelector<HTMLDivElement>('.grid')!;
+
+function otherColour(col: Colour): Colour {
+  switch (col) {
+    case 'red':
+      return 'yellow';
+    case 'yellow':
+      return 'red';
+  }
+}
+
+function createCircleElement(colour: Colour): HTMLDivElement {
+  const ele = document.createElement('div');
+  ele.classList.add('token');
+  ele.classList.add(`token-${colour}`);
+  return ele;
+}
+
+function createWinMessage(colour: string) {
+  const ele = document.createElement('h2');
+  ele.classList.add('win-message');
+  ele.innerText = `${colour} won!`;
+  gridContainer.appendChild(ele);
+
+  sideToPlayEle.remove();
+}
+
+function addColourToColumn(idx: number, colour: Colour): boolean {
+  if (grid[idx].length >= 6) {
+    return false;
+  }
+
+  const col = getColumnElement(idx);
+  col.appendChild(createCircleElement(colour));
+  grid[idx].push(colour);
+  console.table(grid);
+  if (grid[idx].length >= 6) {
+    col.classList.add('full');
+  }
+
+  return true;
+}
+
+function availableColumns(): number[] {
+  const available: number[] = [];
+  for (let i = 0; i < grid.length; i++) {
+    const col = grid[i];
+    if (col.length < 6) {
+      available.push(i);
+    }
+  }
+  return available;
+}
+
+function checkForWinHorizontal(): Colour | null {
+  for (let row = 0; row < 6; row++) {
+    for (let startCol = 0; startCol < 4; startCol++) {
+      const startColumn = grid[startCol];
+      if (startColumn.length < (row+1)) {
+        continue;
+      }
+      const initialColour = grid[startCol][row];
+      let ok = true;
+      for (let i = 1; i <= 3; i++) {
+        const c = grid[startCol+i];
+        if (c.length < (row+1)) {
+          ok = false;
+          break;
+        }
+        if (c[row] != initialColour) {
+          ok = false;
+          break;
+        }
+      }
+      if (ok) {
+        return initialColour;
+      }
+    }
+  }
+
+  return null;
+}
+
+function checkForWinVertical(): Colour | null {
+  outer:for (let col = 0; col < 7; col++) {
+    const column = grid[col];
+    for (let startRow = 0; startRow < 6; startRow++) {
+      if (column.length < (startRow + 4)) {
+        continue outer;
+      }
+      const window = column.slice(startRow, startRow + 4 + 1);
+      const firstCol = window[0];
+      for (const col of window) {
+        if (col !== firstCol) {
+          continue outer;
+        }
+      }
+      return firstCol;
+    }
+  }
+  return null;
+}
+
+function checkForWinDiagonalPositive(): Colour | null {
+  outer:for (let startCol = 0; startCol < 4; startCol++) {
+    const column = grid[startCol];
+    for (let startRow = 0; startRow < 3; startRow++) {
+      if (column.length < startRow) {
+        continue outer;
+      }
+      const initial = column[startRow];
+      for (let i = 1; i < 4; i++) {
+        const newColumn = grid[startCol + i];
+        if (newColumn.length < startRow + i) {
+          continue outer;
+        }
+        if (newColumn[startRow + i] !== initial) {
+          continue outer;
+        }
+      }
+      return initial;
+    }
+  }
+  return null;
+}
+
+function checkForWinDiagonalNegative(): Colour | null {
+  outer:for (let startCol = 0; startCol < 4; startCol++) {
+    const column = grid[grid.length - 1 - startCol];
+    for (let startRow = 0; startRow < 3; startRow++) {
+      if (column.length < startRow) {
+        continue outer;
+      }
+      const initial = column[startRow];
+      for (let i = 1; i < 4; i++) {
+        const newColumn = grid[grid.length - 1 - (startCol + i)];
+        if (newColumn.length < startRow + i) {
+          continue outer;
+        }
+        if (newColumn[startRow + i] !== initial) {
+          continue outer;
+        }
+      }
+      return initial;
+    }
+  }
+  return null;
+}
+
+function checkWin(): Colour | null {
+  return checkForWinHorizontal() || checkForWinVertical() || checkForWinDiagonalPositive() || checkForWinDiagonalNegative();
+}
+
+function checkDraw(): boolean {
+  for (const col of grid) {
+    if (col.length < 6) return false;
+  }
+  return true;
+}
+
+let grid: Grid = [[], [], [], [], [], [], []];
+let sideToPlay: Colour = 'red';
+const sideToPlayEle = document.querySelector<HTMLHeadingElement>('#to-move')!;
+let hasWon = false;
+let hasDraw = false;
+const ALLOW_AI = true;
+const AI_BATTLE = true;
+
+function updateToPlay() {
+  gridEle.classList.remove(sideToPlay);
+  sideToPlay = otherColour(sideToPlay);
+  gridEle.classList.add(sideToPlay);
+  sideToPlayEle.innerText = `${sideToPlay} to play`;
+}
+
+function detatchEventListeners() {
+  for (let i = 0; i < 7; i++) {
+    const col = getColumnElement(i);
+    col.onclick = null;
+  }
+}
+
+function triggerAI() {
+  setTimeout(() => {
+    if (!ALLOW_AI || hasWon || hasDraw) return;
+    const available = availableColumns();
+    const col = available[Math.floor(Math.random() * available.length)];
+    placeTokenInColumn(col);
+  }, 0);
+}
+
+function gameEnd() {
+  if (AI_BATTLE) {
+    setTimeout(() => {
+      window.location.reload();
+    }, 500);
+  }
+}
+
+function placeTokenInColumn(column: number) {
+  if (addColourToColumn(column, sideToPlay)) {
+    const won = checkWin();
+    if (won) {
+      createWinMessage(won);
+      detatchEventListeners();
+      hasWon = true;
+      gameEnd();
+    } else if (checkDraw()) {
+      createWinMessage('nobody');
+      detatchEventListeners();
+      hasDraw = true;
+      gameEnd();
+    } else {
+      updateToPlay();
+      if (AI_BATTLE || (ALLOW_AI && sideToPlay == 'yellow')) {
+        triggerAI();
+      }
+    }
+  }
+}
+
+function attachEventListeners() {
+  for (let i = 0; i < 7; i++) {
+    const col = getColumnElement(i);
+    col.onclick = () => {
+      placeTokenInColumn(i);
+    };
+  }
+}
+
+function init() {
+  attachEventListeners();
+  updateToPlay();
+  updateToPlay();
+  if (AI_BATTLE) {
+    triggerAI();
+  }
+}
+
+init();
